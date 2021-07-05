dbacb41af32f24cd105aaf91fbb9de54cd59fb2b
lizka.flex@outlook.com <lizka.flex@outlook.com>
2021-07-05 09:49:31 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Экзамен
+{
+    public class Class1
+    {
+        string s = "";
+        public void Crt(string path)
+        {
+            List<Rbt> ret;
+            List<Rbt> ls = Flrd(path);
+            ret = ls.FindAll(x => x.point1 == ls[Minel(ls)].point1);
+            List<List<Rbt>> fncln = new List<List<Rbt>>();
+            foreach (Rbt rb in ret)
+            {
+                Mv(ls, rb);
+                fncln.Add(RtPrs(ls, s));
+                s = "";
+            }
+            int max = fncln[0][0].length, maxind = 0;
+            for (int i = 0; i < ret.Count; i++)
+            {
+                if (FnlMv(fncln[i]) >= max)
+                {
+                    max = FnlMv(fncln[i]);
+                    maxind = i;
+                }
+            }
+            using (StreamWriter sr = new StreamWriter("Вывод.txt"))
+            {
+                foreach (Rbt rb in fncln[maxind])
+                {
+                    sr.WriteLine(rb.point1 + " - " + rb.point2);
+                }
+                sr.WriteLine(max);
+            }
+        }
+        struct Rbt
+        {
+            public int point1;
+            public int point2;
+            public int length;
+            public bool Equals(Rbt obj)
+            {
+                if (this.point1 == obj.point1 && this.point2 == obj.point2 && this.length == obj.length) return true;
+                else return false;
+            }
+            public override string ToString()
+            {
+                return point1.ToString() + " - " + point2.ToString() + " " + length.ToString();
+            }
+        }
+        int Minel(List<Rbt> ls)
+        {
+            int min = ls[0].point1, minind = 0;
+            foreach (Rbt rb in ls)
+            {
+                if (rb.point1 <= min)
+                {
+                    min = rb.point1;
+                    minind = ls.IndexOf(rb);
+                }
+            }
+            return minind;
+        }
+        int Maxel(List<Rbt> ls)
+        {
+            int min = ls[0].point2, maxind = 0;
+            foreach (Rbt rb in ls)
+            {
+                if (rb.point2 >= min)
+                {
+                    min = rb.point1;
+                    maxind = ls.IndexOf(rb);
+                }
+            }
+            return maxind;
+        }
+        int Mv(List<Rbt> ls, Rbt minel)
+        {
+            int ret = 0;
+            Rbt rb = ls.Find(x => x.point1 == minel.point1 && x.point2 == minel.point2);
+            s += rb.point1.ToString() + "-" + rb.point2.ToString();
+            if (rb.point2 == ls[Maxel(ls)].point2)
+            {
+                s += ";";
+                return rb.length;
+            }
+            else
+            {
+                for (int i = 0; i < ls.Count; i++)
+                {
+                    if (ls[i].point1 == rb.point2)
+                    {
+                        s += ",";
+                        ret = Mv(ls, ls[i]) + rb.length;
+                    }
+                }
+            }
+            return ret;
+        }
+        int Mv(List<Rbt> ls, Rbt minel, StreamWriter sr)
+        {
+            int ret = 0;
+            Rbt rb = ls.Find(x => x.point1 == minel.point1 && x.point2 == minel.point2);
+            sr.WriteLine(rb.point1 + " - " + rb.point2);
+            if (rb.point2 == ls[Maxel(ls)].point2)
+            {
+                return rb.length;
+            }
+            else
+            {
+                for (int i = 0; i < ls.Count; i++)
+                {
+                    if (ls[i].point1 == rb.point2)
+                    {
+                        ret = Mv(ls, ls[i], sr) + rb.length;
+                    }
+                }
+            }
+            return ret;
+        }
+        List<Rbt> Flrd(string path)
+        {
+            List<Rbt> ret = new List<Rbt>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string[] str1 = sr.ReadLine().Split(';');
+                    string[] str2 = str1[0].Split('-');
+                    ret.Add(new Rbt { point1 = Convert.ToInt32(str2[0]), point2 = Convert.ToInt32(str2[1]), length = Convert.ToInt32(str1[1]) });
+                }
+            }
+            return ret;
+        }
+        List<Rbt> RtPrs(List<Rbt> ls, string s)
+        {
+            List<List<Rbt>> ret = new List<List<Rbt>>();
+            string[] str1 = s.Split(';');
+            foreach (string st1 in str1)
+            {
+                if (st1 != "")
+                {
+                    ret.Add(new List<Rbt>());
+                    string[] str2 = st1.Split(',');
+                    foreach (string st2 in str2)
+                    {
+                        if (st2 != "")
+                        {
+                            string[] str3 = st2.Split('-');
+                            ret[ret.Count - 1].Add(ls.Find(x => x.point1 == Convert.ToInt32(str3[0]) && x.point2 == Convert.ToInt32(str3[1])));
+                        }
+                    }
+                }
+            }
+            foreach (List<Rbt> l in ret)
+            {
+                if (l[0].point1 != ls[Minel(ls)].point1)
+                {
+                    foreach (List<Rbt> l1 in ret)
+                    {
+                        if (l1[0].point1 == ls[Minel(ls)].point1)
+                        {
+                            l.InsertRange(0, l1.FindAll(x => l1.IndexOf(x) <= l1.FindIndex(y => y.point2 == l[0].point1)));
+                        }
+                    }
+                }
+            }
+            int max = ret[0][0].length, maxind = 0;
+            for (int i = 0; i < ret.Count; i++)
+            {
+                if (FnlMv(ret[i]) >= max)
+                {
+                    max = FnlMv(ret[i]);
+                    maxind = i;
+                }
+            }
+            return ret[maxind];
+        }
+        int FnlMv(List<Rbt> ls)
+        {
+            int ret = 0;
+            foreach (Rbt rb in ls)
+            {
+                ret += rb.length;
+            }
+            return ret;
+        }
+    }
+}
+  
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Экзамен
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int otv = 1;
+            while (otv != 0)
+            {
+                Console.WriteLine("Выберите задачу:");
+                Console.WriteLine(" 1 - кртический путь.");
+                int nom = Convert.ToInt32(Console.ReadLine());
+                if (nom == 1)
+                {
+                    Class1 c1 = new Class1();
+                    c1.Crt("Ввод.csv");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод!");
+                }
+                Console.WriteLine("Продолжить работу? да - 1, нет - 0");
+                otv = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
